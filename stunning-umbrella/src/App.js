5b4987f672e3f6fc33a9294d4bef56c26e4ecade
kimdahey <claire_kim1@brown.edu>
2019-11-05 23:52:26 -0500

    basic routes

--- stunning-umbrella/src/App.js
+++ stunning-umbrella/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './Home.jsx';
+import About from './About.jsx';
+import Projects from './Projects.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="header">
+          <h1>claire kim</h1>
+          <div className="nav">
+            <Link to="/" className="nav-item">home</Link>
+            <Link to="/about" className="nav-item">about</Link>
+            <Link to="/projects" className="nav-item">projects</Link>
+          </div>
+        </header>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ stunning-umbrella/src/Projects.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+    render() {
+        return (
+            <div className="projects">
+                <h1>toohoo</h1>
+            </div>
+        )
+    }
+}
+
+export default About;
